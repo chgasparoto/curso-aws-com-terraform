715633a2a176ce6eb18fc119e32fb77ba39ea9c9
Cleber Gasparoto <chgasparoto@gmail.com>
2021-01-14 18:55:28 +0100

    add change/destroy class code

--- 01-terraform-basico/01-primeiro-script/main.tf
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -1,24 +1,26 @@
 terraform {
   required_version = "0.14.4"
+
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "3.23.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "eu-central-1"
-  profile = "tf013"
+  region = "eu-central-1" # Brasil -> us-east-1
+  profile = "tf014"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "meu-bucket-000111222333444555"
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-123123455745642342342"
+  acl    = "private"
 
   tags = {
-    Owner       = "Cleber Gasparoto"
-    Environment = "dev"
-    ManagedBy   = "Terraform"
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby = "Terraform"
   }
 }
