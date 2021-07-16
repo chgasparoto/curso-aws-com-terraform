7554e6e69a46aa41663998c6904869fed76fda8d
Cleber Gasparoto <chgasparoto@gmail.com>
2021-07-16 23:15:51 +0200

    terraform fmt

--- 06-ecs-fargate/terraform/ecr/locals.tf
+++ 06-ecs-fargate/terraform/ecr/locals.tf
@@ -1,5 +1,5 @@
 locals {
   app_name  = "${var.app_name}-${var.env}"
-  file_hash = sha1(join("", [for f in fileset(var.app_folder, "*"): filesha1("${var.app_folder}/${f}")]))
+  file_hash = sha1(join("", [for f in fileset(var.app_folder, "*") : filesha1("${var.app_folder}/${f}")]))
 
 }
